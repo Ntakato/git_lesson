eea39bef8a17b1fb6964fb293743755afd07c4e2
S.Gunji <86037846+Gunji76@users.noreply.github.com>
2024-08-01 17:01:44 +0900

    バグ修正 issue5 (#12)

--- sample/fizzBuzz/fizzbuzz.py
+++ sample/fizzBuzz/fizzbuzz.py
@@ -1,6 +1,8 @@
 for i in range(1, 101):
-    if i % 3 == 0:
-        print('Fiz')
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
     elif i % 5 == 0:
         print('Buzz')
     else:
