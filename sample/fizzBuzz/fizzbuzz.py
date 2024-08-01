e257579766406efe5a79c91149b99850577d6553
S.Gunji <gunji@sevensix.co.jp>
2024-08-01 11:27:07 +0900

    add user specified function

--- sample/fizzBuzz/fizzbuzz.py
+++ sample/fizzBuzz/fizzbuzz.py
@@ -1,7 +1,14 @@
+num1, str1 = input ('input number, string\n').split()
+num2, str2 = input().split()
+print ('-----')
+
+num1 = int(num1)
+num2 = int(num2)
+
 for i in range(1, 101):
-    if i % 3 == 0:
-        print('Fiz')
-    elif i % 5 == 0:
-        print('Buzz')
+    if i % int(num1) == 0:
+        print(str1)
+    elif i % int(num2) == 0:
+        print(str2)
     else:
         print(i)
